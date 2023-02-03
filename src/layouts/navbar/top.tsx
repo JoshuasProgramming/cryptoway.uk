3d87503235017903f66555d6dc3df4519f3a5949
Joshua Thomas <90353177+JoshuasProgramming@users.noreply.github.com>
2023-02-03 12:20:22 +0000

    made the navbar top deletable with scss and state

--- src/layouts/navbar/top.tsx
+++ src/layouts/navbar/top.tsx
@@ -1,12 +1,21 @@
-//import react
-import React from 'react'
+//import useState
+import { useState } from 'react'
 
 function Top() {
+
+  const [navbarTop, setNavbarTop] = useState(true);
+
   return (
+    navbarTop == true ? 
     <section className="navbar-top">
         <h1>Sign up to start building wealth</h1>
-        <h1 className='navbar-top-x-btn'>X</h1>
+        <h2 className='navbar-top-x-btn' id="navbar_top" onClick={() => setNavbarTop(false)}>X</h2>
     </section>
+
+    :
+
+    false
+    
   )
 }
 
