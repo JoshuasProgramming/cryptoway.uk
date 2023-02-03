23653e5bf670cff661ec4381da9350184ef5f5fb
Joshua Thomas <90353177+JoshuasProgramming@users.noreply.github.com>
2023-02-03 11:50:54 +0000

    added x button on navbar > top.tsx

--- src/layouts/navbar/top.tsx
+++ src/layouts/navbar/top.tsx
@@ -5,6 +5,7 @@ function Top() {
   return (
     <section className="navbar-top">
         <h1>Sign up to start building wealth</h1>
+        <h1 className='navbar-top-x-btn'>X</h1>
     </section>
   )
 }
