2fc1d3d4c1c9e6e00771c20e9ea93fcbb4f3a959
Joshua Thomas <90353177+JoshuasProgramming@users.noreply.github.com>
2023-02-07 18:29:00 +0000

    added more styling to hero.tsx

--- src/layouts/homepage/hero.tsx
+++ src/layouts/homepage/hero.tsx
@@ -8,12 +8,13 @@ function Hero() {
 
         <div className="left">
           <div className="top">
+            <a href="" className='top-btn'>Jump Start Your Portfolio 	  &#10148;</a>
             <h1>Buy & Trade<br></br>Cryptocurrencies</h1>
             <p>Join the millions of people who trust us to securely buy, sell, and store their cryptocurrencies.</p>
 
             <div className="btn-container">
-            <a href="" className='pd-right'>Sign Up</a>
-            <a href="">Learn More</a>
+              <a href=""className='pd-right btns' >Sign Up</a>
+              <a href=""className='btns'>Learn More</a>
             </div>
 
           </div>
@@ -23,7 +24,7 @@ function Hero() {
         </div>
 
         <div className="right">
-            <img src={hero_image} alt="" />
+            <img src={hero_image} alt={hero_image} />
         </div>
 
     </section>
