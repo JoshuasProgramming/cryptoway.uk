02f8ee1377b4a74764420d889b0890cd8b0fba35
Joshua Thomas <90353177+JoshuasProgramming@users.noreply.github.com>
2023-02-17 14:12:27 +0000

    created need_help.tsx component

--- src/layouts/homepage/need_help.tsx
+++ src/layouts/homepage/need_help.tsx
@@ -1,5 +1,11 @@
 import React from 'react'
 
+//import icons
+import supportTeam from './icons/icons8-people-100.png';
+import security from './icons/icons8-password-100.png';
+import faqs from './icons/icons8-questions-100.png';
+
+
 function Need_help() {
   return (
     <section className="need-help">
@@ -11,7 +17,7 @@ function Need_help() {
 
         <div className="need-help-parent">
             <div className="need-help-children">
-                <div className="left"><img src="" alt="" /></div>
+                <div className="left"><img src={faqs} alt="" /></div>
                 <div className="right">
                     <h3>FAQs</h3>
                     <p>Before reaching out to our support team, we recommend checking our frequently asked questions (FAQs) page. </p>
@@ -20,7 +26,7 @@ function Need_help() {
             </div>
 
             <div className="need-help-children">
-                <div className="left"><img src="" alt="" /></div>
+                <div className="left"><img src={supportTeam} alt="" /></div>
                 <div className="right">
                     <h3>SUPPORT TEAMS</h3>
                     <p>You can contact us via email, phone, or live chat, and we'll do our best to help you as quickly as possible.</p>
@@ -29,7 +35,7 @@ function Need_help() {
             </div>
 
             <div className="need-help-children">
-                <div className="left"><img src="" alt="" /></div>
+                <div className="left"><img src={security} alt="" /></div>
                 <div className="right">
                     <h3>SECURITY</h3>
                     <p>At CryptoWay, we take the security of our users' assets very seriously.</p>
