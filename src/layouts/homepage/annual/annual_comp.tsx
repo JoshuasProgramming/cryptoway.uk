14f2223170f6e80188af98c250d4bff2e2945b2d
Joshua Thomas <90353177+JoshuasProgramming@users.noreply.github.com>
2023-02-17 15:07:45 +0000

    filled in html contents for annual_comp.tsx

--- src/layouts/homepage/annual/annual_comp.tsx
+++ src/layouts/homepage/annual/annual_comp.tsx
@@ -2,11 +2,11 @@ import React from 'react'
 
 function annual_comp(props:any) {
   return (
-    <div>
-        {props.image}
-        {props.name}
-        {props.annual}
-    </div>
+    <section className='annual-children'>
+        <img src={props.image} alt={props.name + " image"} />
+        <h3>{props.name}</h3>
+        <h1>{props.annual}</h1>
+    </section>
   )
 }
 
