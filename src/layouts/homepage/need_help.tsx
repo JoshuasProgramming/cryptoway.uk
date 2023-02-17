2ac6fcd073127d78b4187cd5550156905364ad5a
Joshua Thomas <90353177+JoshuasProgramming@users.noreply.github.com>
2023-02-17 13:35:31 +0000

    Start styling and text for need-help.tsx

---
+++ src/layouts/homepage/need_help.tsx
@@ -0,0 +1,44 @@
+import React from 'react'
+
+function Need_help() {
+  return (
+    <section className="need-help">
+        {/* (div.need-help-containers>(.left>img>)+.right>h3+p+a)*3 */}
+
+        <div className="need-help-heading">
+            <h1>Need Help?</h1>
+        </div>
+
+        <div className="need-help-parent">
+            <div className="need-help-children">
+                <div className="left"><img src="" alt="" /></div>
+                <div className="right">
+                    <h3></h3>
+                    <p></p>
+                    <a href="">Learn more</a>
+                </div>
+            </div>
+
+            <div className="need-help-children">
+                <div className="left"><img src="" alt="" /></div>
+                <div className="right">
+                    <h3></h3>
+                    <p></p>
+                    <a href="">Learn more</a>
+                </div>
+            </div>
+
+            <div className="need-help-children">
+                <div className="left"><img src="" alt="" /></div>
+                <div className="right">
+                    <h3></h3>
+                    <p></p>
+                    <a href="">Learn more</a>
+                </div>
+            </div>
+        </div>
+    </section>
+  )
+}
+
+export default Need_help;
